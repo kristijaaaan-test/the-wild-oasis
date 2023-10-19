b55188e7a85af780e23d1c5722b3ad7d8ee364fb
Kristijan Horvatić <111249835+kristijaaaan-test@users.noreply.github.com>
2023-10-19 12:27:35 +0200

    Add files via upload

---
+++ 17-the-wild-oasis/src/services/apiCabins.js
@@ -0,0 +1,9 @@
+import supabase from "./supabase";
+
+export async function getCabins() {
+  const { data: cabins, error } = await supabase.from("cabins").select("*");
+
+  if (error) throw new Error(error.message);
+
+  return cabins;
+}
