fdd706e42231bd348214644e402e4c182d6e974c
Kristijan Horvatić <111249835+kristijaaaan-test@users.noreply.github.com>
2023-10-20 12:26:43 +0200

    Delete 17-the-wild-oasis directory

--- 17-the-wild-oasis/src/services/apiCabins.js
+++
@@ -1,9 +0,0 @@
-import supabase from "./supabase";
-
-export async function getCabins() {
-  const { data: cabins, error } = await supabase.from("cabins").select("*");
-
-  if (error) throw new Error(error.message);
-
-  return cabins;
-}
--- 17-the-wild-oasis/src/services/supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://klblktymkrygckvdpdrj.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtsYmxrdHlta3J5Z2NrdmRwZHJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTIxMDI5NzIsImV4cCI6MjAwNzY3ODk3Mn0.y6h1h5S0a2npGJUDwBv_VyoRvJrAxhL8XjWhp2_u9Mg";
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
